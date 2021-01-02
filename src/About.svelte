39e6aea4dd2189671b584302868257fa35a7da52
Logta <takenobu15@gmail.com>
2021-01-02 18:31:31 +0900

    fix navbar

---
+++ src/About.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<!-- Styles -->
+<style>
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</svelte:head>
+
+<main>
+	<h1>曲あてクイズ</h1>
+</main>
--- src/Home.svelte
+++ src/Home.svelte
@@ -7,26 +7,35 @@
     Nav,
     NavItem,
     NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
 	} from "sveltestrap";
+	import Router, {link} from 'svelte-spa-router'
 	import PointCalc from "./PointCalc.svelte";
+	import About from "./About.svelte";
 
-	let csv = "";
+	const routes = {
+    '/': About,
+    '/point/': PointCalc,
+	}
+	let isOpen = false;
 
-let isOpen = false;
-
-function handleUpdate(event) {
-  isOpen = event.detail.isOpen;
-}
+	function handleUpdate(event) {
+	isOpen = event.detail.isOpen;
+	}
 </script>
 
 <style>
 	@import "bootstrap/dist/css/bootstrap.min.css";
 	div#home{
 		padding-top: 25px;
+	}
+	a:link{
+		color:antiquewhite;
+	}
+	a{
+		padding-right: 10px;
+  position: relative;
+  top: 25%;
+		color:antiquewhite;
 	}
 </style>
 
@@ -37,7 +46,7 @@ function handleUpdate(event) {
 		<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
 		  <Nav class="ml-auto" navbar>
 			<NavItem>
-			  <NavLink href="#components/">得点計算</NavLink>
+			  <a href="/point/" use:link>得点計算</a>
 			</NavItem>
 			<NavItem>
 			  <NavLink href="https://github.com/Logta/GuessSongQuiz">GitHub</NavLink>
@@ -46,6 +55,6 @@ function handleUpdate(event) {
 		</Collapse>
 	  </Navbar>
 	  <div id="home">
-		<PointCalc />
+		<Router {routes}/>
 	  </div>
 </main>
